aa74b83394d8276be02918787c717f9c7b541d96
Tharaka Udayanga <tharakaucsc@gmail.com>
2014-01-06 15:37:18 +0530

    initial project version

---
+++ Test.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class Test{
+    public static void main(String args[]){
+        int fact = factorial(8);
+        System.out.println("8! = "+fact);
+    }
+/***************
+* iterative method **
+***************/
+    static int factorial(int n){
+        int result = n;
+        for (int i= n-1; i>0;i--){
+            result *=i;
+        }
+        return result;
+    }
+}
