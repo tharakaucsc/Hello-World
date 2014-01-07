08d975acc42e404d3f2f00d6e73d2ac0113e30cd
Tharaka Udayanga <tharakaucsc@gmail.com>
2014-01-07 15:34:30 +0530

    Added a Header

--- Test.java
+++ Test.java
@@ -1,6 +1,7 @@
 import java.io.*;
 public class Test{
     public static void main(String args[]){
+		System.out.println("Factorial Calculator");
         int fact = factorial(8);
         System.out.println("8! = "+fact);
     }
